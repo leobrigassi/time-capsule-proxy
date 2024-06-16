a37d9e87945882cbaf3d1dddc1925107a887046f
leobrigassi <leo.brigassi@gmail.com>
2024-06-16 21:01:41 +0200

    v1.0.1

--- setup.sh
+++ setup.sh
@@ -5,10 +5,9 @@ if [ "$(basename $(pwd))" == "Time_Capsule_Proxy" ]; then
 else
     echo "Creating Time_Capsule_Proxy directory."
     mkdir -p Time_Capsule_Proxy &&
-    cd Time_Capsule_Proxy
+    cd Time_Capsule_Proxy >/dev/null 2>&1
 fi
 curl -# -sSL https://github.com/leobrigassi/Time_Capsule_Proxy/archive/refs/heads/main.zip > Time_Capsule_Proxy.zip && 
 unzip -j Time_Capsule_Proxy.zip && 
 rm Time_Capsule_Proxy.zip && 
-chmod +x install_Time_Capsule_Proxy.sh && 
-./install_Time_Capsule_Proxy.sh
+chmod +x install_Time_Capsule_Proxy.sh
