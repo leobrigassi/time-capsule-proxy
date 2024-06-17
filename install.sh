d9f48fc4f0750d4470841bdf5e3c31396b169ec1
leobrigassi <leo.brigassi@gmail.com>
2024-06-17 11:26:02 +0200

    v1.0.1

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # Time Capsule Proxy for SmallMediaHub - Updates and readme on https://github.com/leobrigassi/Time_Capsule_Proxy
-if [ "$(basename $(pwd))" == "Time_Capsule_Proxy" ]; then
-    echo "Extracting in Time_Capsule_Proxy directory."
+if [ "$(basename $(pwd))" == "time-capsule-proxy" ]; then
+    echo "Extracting in time-capsule-proxy directory."
 else
     echo "Creating Time_Capsule_Proxy directory."
-    mkdir -p Time_Capsule_Proxy &&
-    cd Time_Capsule_Proxy >/dev/null 2>&1
+    mkdir -p time-capsule-proxy &&
+    cd time-capsule-proxy >/dev/null 2>&1
 fi
 wget https://github.com/leobrigassi/Time_Capsule_Proxy/archive/refs/heads/main.tar.gz &&
 tar -xzf main.tar.gz --strip-components=1 && rm main.tar.gz &&
